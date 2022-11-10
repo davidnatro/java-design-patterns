8918b9c8b7ee74555295c08740d90d0335000806
David <53772525+davidnatro@users.noreply.github.com>
2022-11-10 19:31:22 +0600

    Update Transport.java

--- src/main/java/org/designpatterns/factory_method/Transport.java
+++ src/main/java/org/designpatterns/factory_method/Transport.java
@@ -1,8 +1,6 @@
 package org.designpatterns.factory_method;
 
 public abstract class Transport {
-
-
     public String deliver() {
         return "Your order will be delivered by " + this + "!";
     }
